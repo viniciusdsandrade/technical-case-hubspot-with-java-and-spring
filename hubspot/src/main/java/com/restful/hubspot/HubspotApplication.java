52a5311b67f39d1da11f51814711216a51ad9898
Vinícius Andrade <vinicius_andrade2010@hotmail.com>
2025-04-06 18:43:21 -0300

    feat: initializer

---
+++ hubspot/src/main/java/com/restful/hubspot/HubspotApplication.java
@@ -0,0 +1,13 @@
+package com.restful.hubspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubspotApplication.class, args);
+	}
+
+}
---
+++ hubspot/src/test/java/com/restful/hubspot/HubspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful.hubspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
